076e539abfcfe23a5eb682dea51f5982020c6344
Brandon Walsh <bmwalshy@gmail.com>
2025-12-16 23:20:52 -0500

    Day 2 part 1

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/Users/brandon/Documents/advent-of-code-2025/day2/target/debug/day2: /Users/brandon/Documents/advent-of-code-2025/day2/src/main.rs
--- day2/target/debug/deps/day2-d6a651c7d653254d.d
+++
@@ -1,5 +0,0 @@
-/Users/brandon/Documents/advent-of-code-2025/day2/target/debug/deps/day2-d6a651c7d653254d.d: src/main.rs
-
-/Users/brandon/Documents/advent-of-code-2025/day2/target/debug/deps/day2-d6a651c7d653254d: src/main.rs
-
-src/main.rs:
